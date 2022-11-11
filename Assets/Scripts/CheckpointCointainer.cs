ddf5a7fe994e61104807a98528f08ad53f8a0891
Ahmad Adillaumam <wvwnumam@gmail.com>
2022-11-11 19:14:32 +0700

    Import project

---
+++ Assets/Scripts/CheckpointCointainer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CheckpointCointainer : MonoBehaviour {
+
+	int currentCheckpoint = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (CheckpointController checkpoint in this.GetComponentsInChildren<CheckpointController>())
+        {
+            checkpoint.onHitByPlayer = (int checkpointId) => OnReachCheckpoint(checkpointId);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnReachCheckpoint(int checkpointId)
+    {
+        if (checkpointId == currentCheckpoint + 1) {
+            currentCheckpoint++;
+        }
+        else if (checkpointId == 1 && currentCheckpoint == 4) {
+            if (Data.timer < Data.bestTime)
+            {
+                Data.bestTime = Data.timer;
+                Data.isBestTime = true;
+            }
+            currentCheckpoint = 1;
+            Data.timer = 0;
+        }
+    }
+}
